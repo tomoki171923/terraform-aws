2b9df50286f3f6f908fe6040d5c32c478a19d338
tomoki <tomoki171923@gmail.com>
2021-04-25 18:38:37 +0900

    delete unneccesary comment

--- lambda/provider.tf
+++ lambda/provider.tf
@@ -1,8 +1,4 @@
 provider "aws" {
-  #　Access keyを使っても酔いが、AWSのProfileの方が便利且つ、Secret Access Keyを間違ってGitにコミットしてしまう恐れがなく安全。
-  # access_key = var.aws_access_key
-  # secret_key = var.aws_secret_key
-
   region  = var.aws_region
   profile = var.aws_profile
   version = "~> 3.35"
