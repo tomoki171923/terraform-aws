a1f3a7f5c54ca47b2da48b979c44569b0197b71a
tomoki <tomoki171923@gmail.com>
2021-04-29 12:55:09 +0900

    refacter iam modeule

---
+++ iam/modules/variables.tf
@@ -0,0 +1,44 @@
+# *********** Variables *********** #
+variable "aws_default_policies" {
+  default = {
+    # Admin
+    AdministratorAccess = "arn:aws:iam::aws:policy/AdministratorAccess"
+    AWSSupportAccess = "arn:aws:iam::aws:policy/AWSSupportAccess"
+
+    # IAM
+    IAMUserChangePassword = "arn:aws:iam::aws:policy/IAMUserChangePassword"
+
+    # API Gateway
+    AmazonAPIGatewayAdministrator = "arn:aws:iam::aws:policy/AmazonAPIGatewayAdministrator"
+
+    # Lambda
+    AWSLambda_FullAccess = "arn:aws:iam::aws:policy/AWSLambda_FullAccess"
+    AWSLambdaExecute = "arn:aws:iam::aws:policy/AWSLambdaExecute"
+    AWSLambdaRole = "arn:aws:iam::aws:policy/service-role/AWSLambdaRole"
+    AWSLambda_ReadOnlyAccess = "arn:aws:iam::aws:policy/AWSLambda_ReadOnlyAccess"
+
+    # DynamoDB
+    AmazonDynamoDBFullAccess = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+    AmazonDynamoDBReadOnlyAccess = "arn:aws:iam::aws:policy/AmazonDynamoDBReadOnlyAccess"
+
+    # S3
+    AmazonS3FullAccess = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+    AmazonS3ReadOnlyAccess = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+
+    # Cloud watch
+    CloudWatchFullAccess = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
+    CloudWatchLogsFullAcces = "arn:aws:iam::aws:policy/CloudWatchLogsFullAcces"
+    CloudWatchEventsReadOnlyAccess = "arn:aws:iam::aws:policy/CloudWatchEventsReadOnlyAccess"
+    CloudWatchReadOnlyAccess = "arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess"
+
+    # CloudFront
+    CloudFrontFullAccess = "arn:aws:iam::aws:policy/CloudFrontFullAccess"
+
+    # DataPipeline
+    AWSDataPipeline_FullAccess = "arn:aws:iam::aws:policy/AWSDataPipeline_FullAccess"
+
+    # SES
+    AmazonSESFullAccess = "arn:aws:iam::aws:policy/AmazonSESFullAccess"
+
+  }
+}
