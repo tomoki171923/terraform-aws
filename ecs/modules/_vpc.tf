fd00fbe14a7a084a8ee92b451613239c51022c97
tomoki <tomoki171923@gmail.com>
2021-06-23 17:30:48 +0900

    add ecs modules

---
+++ ecs/modules/_vpc.tf
@@ -0,0 +1,154 @@
+locals {
+  base_name             = "ecs-test"
+  region                = "ap-northeast-1"
+  vpc_cidr              = "10.0.0.0/16"
+  public_subnet_1_cidr  = "10.0.1.0/24"
+  public_subnet_2_cidr  = "10.0.2.0/24"
+  private_subnet_1_cidr = "10.0.3.0/24"
+  private_subnet_2_cidr = "10.0.4.0/24"
+}
+
+
+resource "aws_vpc" "ecs-test-vpc" {
+  cidr_block           = local.vpc_cidr
+  enable_dns_hostnames = true
+
+  tags = {
+    Name        = "${local.base_name}-vpc"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+resource "aws_subnet" "public-subnet-1" {
+  cidr_block        = local.public_subnet_1_cidr
+  vpc_id            = aws_vpc.ecs-test-vpc.id
+  availability_zone = "${local.region}a"
+
+  tags = {
+    Name        = "${local.base_name}-public-1"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+resource "aws_subnet" "public-subnet-2" {
+  cidr_block        = local.public_subnet_2_cidr
+  vpc_id            = aws_vpc.ecs-test-vpc.id
+  availability_zone = "${local.region}c"
+
+  tags = {
+    Name        = "${local.base_name}-public-2"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+resource "aws_subnet" "private-subnet-1" {
+  cidr_block        = local.private_subnet_1_cidr
+  vpc_id            = aws_vpc.ecs-test-vpc.id
+  availability_zone = "${local.region}a"
+
+  tags = {
+    Name        = "${local.base_name}-private-1"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+resource "aws_subnet" "private-subnet-2" {
+  cidr_block        = local.private_subnet_2_cidr
+  vpc_id            = aws_vpc.ecs-test-vpc.id
+  availability_zone = "${local.region}c"
+
+  tags = {
+    Name        = "${local.base_name}-private-2"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.ecs-test-vpc.id
+  tags = {
+    Name        = "${local.base_name}-public-rt"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+resource "aws_route_table" "private-route-table" {
+  vpc_id = aws_vpc.ecs-test-vpc.id
+  tags = {
+    Name        = "${local.base_name}-private-rt"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+resource "aws_route_table_association" "public-route-1-association" {
+  route_table_id = aws_route_table.public-route-table.id
+  subnet_id      = aws_subnet.public-subnet-1.id
+}
+
+resource "aws_route_table_association" "public-route-2-association" {
+  route_table_id = aws_route_table.public-route-table.id
+  subnet_id      = aws_subnet.public-subnet-2.id
+}
+
+resource "aws_route_table_association" "private-route-1-association" {
+  route_table_id = aws_route_table.private-route-table.id
+  subnet_id      = aws_subnet.private-subnet-1.id
+}
+
+resource "aws_route_table_association" "private-route-2-association" {
+  route_table_id = aws_route_table.private-route-table.id
+  subnet_id      = aws_subnet.private-subnet-2.id
+}
+
+resource "aws_eip" "elastic-ip-for-nat-gw" {
+  vpc                       = true
+  associate_with_private_ip = "10.0.0.5"
+
+  tags = {
+    Name        = "${local.base_name}-eip"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_nat_gateway" "nat-gw" {
+  allocation_id = aws_eip.elastic-ip-for-nat-gw.id
+  subnet_id     = aws_subnet.public-subnet-1.id
+
+  tags = {
+    Name        = "${local.base_name}-natgw"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+
+  depends_on = [aws_eip.elastic-ip-for-nat-gw]
+}
+
+resource "aws_route" "nat-gw-route" {
+  route_table_id         = aws_route_table.private-route-table.id
+  nat_gateway_id         = aws_nat_gateway.nat-gw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.ecs-test-vpc.id
+  tags = {
+    Name        = "${local.base_name}-igw"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+resource "aws_route" "public-internet-igw-route" {
+  route_table_id         = aws_route_table.public-route-table.id
+  gateway_id             = aws_internet_gateway.igw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
---
+++ ecs/modules/outputs.tf
@@ -0,0 +1,22 @@
+
+output "vpc" {
+  value = {
+    ecs-test-vpc                = aws_vpc.ecs-test-vpc
+    public-subnet-1             = aws_subnet.public-subnet-1
+    public-subnet-2             = aws_subnet.public-subnet-2
+    private-subnet-1            = aws_subnet.private-subnet-1
+    private-subnet-2            = aws_subnet.private-subnet-2
+    public-route-table          = aws_route_table.public-route-table
+    private-route-table         = aws_route_table.private-route-table
+    public-route-1-association  = aws_route_table_association.public-route-1-association
+    public-route-2-association  = aws_route_table_association.public-route-2-association
+    private-route-1-association = aws_route_table_association.private-route-1-association
+    private-route-2-association = aws_route_table_association.private-route-2-association
+    elastic-ip-for-nat-gw       = aws_eip.elastic-ip-for-nat-gw
+    nat-gw                      = aws_nat_gateway.nat-gw
+    nat-gw-route                = aws_route.nat-gw-route
+    igw                         = aws_internet_gateway.igw
+    public-internet-igw-route   = aws_route.public-internet-igw-route
+
+  }
+}
