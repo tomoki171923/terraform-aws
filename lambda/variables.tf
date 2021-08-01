48223d8c6d35919b9a53b95a895cc6e13d0929b8
tomoki <tomoki171923@gmail.com>
2021-08-02 08:06:56 +0900

    refacter

--- lambda/variables.tf
+++ lambda/variables.tf
@@ -1,8 +1,16 @@
 # *********** Variables *********** #
-variable "aws_region" {}
-variable "aws_profile" {}
+variable "aws_region" {
+  type        = string
+  description = "AWS region"
+}
+
+variable "aws_profile" {
+  type        = string
+  description = "AWS profile name"
+}
 
 variable "alias" {
+  type        = string
   description = "Please select lambda alias between [dev, st, pro]."
   validation {
     condition     = var.alias == "dev" || var.alias == "st" || var.alias == "pro"
