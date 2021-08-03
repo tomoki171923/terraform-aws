57ab5e6f0c42cb7269bcee2584fd5e1b3307dcab
tomoki <tomoki171923@gmail.com>
2021-08-04 05:17:54 +0900

    add rds module

---
+++ rds/variables.tf
@@ -0,0 +1,11 @@
+# *********** Variables *********** #
+variable "aws_region" {
+  type        = string
+  description = "AWS region"
+}
+
+variable "aws_profile" {
+  type        = string
+  description = "AWS profile name"
+}
+
