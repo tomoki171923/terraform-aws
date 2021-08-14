52d07e0d1b001d1b5a29805e508767f209dbeeeb
tomoki <tomoki171923@gmail.com>
2021-08-15 03:55:48 +0900

    refacter route53

--- route53/modules/outputs.tf
+++ route53/modules/outputs.tf
@@ -1,15 +1,15 @@
 output "zone" {
   value = {
     main    = aws_route53_zone.main
-    sub-dev = aws_route53_zone.sub-dev
+    # sub-dev = aws_route53_zone.sub-dev
   }
 }
 
 output "record" {
   value = {
-    dev-ns          = aws_route53_record.dev-ns
     main-a          = aws_route53_record.main-a
     cert_validation = aws_route53_record.cert_validation
+    # dev-ns          = aws_route53_record.dev-ns
   }
 }
 
