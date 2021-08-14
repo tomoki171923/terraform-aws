bb35f99418a891d9c0625f5069f00c638c278b1d
tomoki <tomoki171923@gmail.com>
2021-08-15 03:50:38 +0900

    refacter lambda module

--- lambda/modules/outputs.tf
+++ lambda/modules/outputs.tf
@@ -3,11 +3,7 @@ output "function" {
   value = {
     hello_world = {
       setting = module.function_hello_world
-      alias = {
-        dev = module.alias_hello_world_dev
-        st  = module.alias_hello_world_st
-        pro = module.alias_hello_world_pro
-      }
+      alias   = module.alias_hello_world
     }
   }
 }
