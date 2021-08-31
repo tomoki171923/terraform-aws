96ec1012e5b86c3704e62c10c9a9b95eb49b3174
tomoki <tomoki171923@gmail.com>
2021-08-31 21:28:22 +0900

    format

--- iam/backend.tf
+++ iam/backend.tf
@@ -4,8 +4,8 @@ terraform {
 
   # backendはterraform{}ブロック内に定義される
   backend "s3" {
-    bucket = "infra-dev-terraform" #このS3 bucketが先に作られている必要がある
-    key    = "iam/terraform.tfstate"      # .tfstateをS3 bucket内にObjectとして保存
+    bucket = "infra-dev-terraform"   #このS3 bucketが先に作られている必要がある
+    key    = "iam/terraform.tfstate" # .tfstateをS3 bucket内にObjectとして保存
     region = "ap-northeast-1"
     #dynamodb_table = "infra-production-terraform-state-lock" # dynamoDBを使ってState Lockを有効化(先に作成する必要がある)
     encrypt = true
