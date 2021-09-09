b873bcc2f4e374cf0937e0fc51cb5d99fee3c122
tomoki <tomoki171923@gmail.com>
2021-09-10 00:12:27 +0900

    add kinesis module

--- kinesis/modules/data.tf
+++ kinesis/modules/data.tf
@@ -1,3 +1,6 @@
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
+
 data "terraform_remote_state" "es" {
   backend = "s3"
   config = {
@@ -13,5 +16,17 @@ data "terraform_remote_state" "s3" {
     bucket = "infra-develop-terraform"
     key    = "s3/terraform.tfstate"
     region = "ap-northeast-1"
+  }
+}
+
+# firehose policiy
+data "template_file" "policy" {
+  template = file("${path.module}/policies/KinesisFirehoseServicePolicy.json")
+  vars = {
+    aws_region     = data.aws_region.current.name
+    aws_account    = data.aws_caller_identity.current.account_id
+    es_domain_name = local.es_state.domain.domain_name
+    es_arn         = local.es_state.domain.arn
+    s3_bucket_arn  = local.s3_state.bucket.tf-test-private-bucket.s3_bucket_arn
   }
 }
