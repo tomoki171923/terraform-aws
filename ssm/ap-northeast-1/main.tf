eea25fae20bff9e12e5cf60051de49ba6aec8ab7
tomoki <tomoki171923@gmail.com>
2021-11-27 18:05:15 +0900

    tmp

--- ssm/ap-northeast-1/main.tf
+++ ssm/ap-northeast-1/main.tf
@@ -1,16 +1,24 @@
+data "aws_ecr_repository" "myecr" {
+  name = "myecr"
+}
+locals {
+  base_name = "SampleSSM"
+}
+
+
 module "network" {
   source    = "../modules/network"
-  base_name = "SampleSSM"
+  base_name = local.base_name
 }
 
 module "iam" {
   source    = "../modules/iam"
-  base_name = "SampleSSM"
+  base_name = local.base_name
 }
 
 module "ec2" {
   source                 = "../modules/ec2"
-  base_name              = "SampleSSM"
+  base_name              = local.base_name
   iam_instance_profile   = module.iam.instance_profile.ssm_instance_profile.name
   subnet_id              = module.network.vpc.private_subnets[0]
   vpc_security_group_ids = [module.network.security_group.ssm2ec2.id]
@@ -20,21 +28,17 @@ module "ec2" {
   ]
 }
 
-data "aws_ecr_repository" "myecr" {
-  name = "myecr"
-}
-
 module "ecs" {
   source             = "../modules/ecs"
-  cluster_name       = "SampleSSM"
-  service_name       = "SampleSSM"
-  task_name          = "SampleSSM"
+  cluster_name       = "${local.base_name}Cluster"
+  service_name       = "${local.base_name}Service"
+  task_name          = "${local.base_name}Task"
   task_role_arn      = module.iam.instance_profile.ssm_instance_profile.name
   execution_role_arn = module.iam.instance_profile.ssm_instance_profile.name
   subnets            = module.network.vpc.private_subnets
   security_groups    = [module.network.security_group.ssm2ec2.id]
   repository_url     = data.aws_ecr_repository.myecr.repository_url
-  container_name     = "SampleSSMcontainer"
+  container_name     = "${local.base_name}Container"
   container_port     = "80"
   container_tag      = "1.0.0"
   depends_on = [
