811b90770e3624d7ef919fb2263b198c4d712d3a
tomoki <tomoki171923@gmail.com>
2022-02-10 21:15:18 +0900

    add security_group resource into sample workspace

--- sample/modules/outputs.tf
+++ sample/modules/outputs.tf
@@ -7,3 +7,12 @@ output "subnet" {
     vpc_endpoint_c = aws_subnet.vpc_endpoint_c
   }
 }
+output "security_group" {
+  value = {
+    public    = aws_security_group.public
+    web       = aws_security_group.web
+    private   = aws_security_group.private
+    ssm2vpcep = aws_security_group.ssm2vpcep
+    ssm2ec2   = aws_security_group.ssm2ec2
+  }
+}
