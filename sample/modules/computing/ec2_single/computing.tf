e8a2adc3bab28164efcd6f262c3620f9e5642d73
tomoki <tomoki171923@gmail.com>
2022-02-11 16:26:30 +0900

    remove user_data

--- sample/modules/computing/ec2_single/computing.tf
+++ sample/modules/computing/ec2_single/computing.tf
@@ -5,11 +5,6 @@ resource "aws_instance" "this" {
   subnet_id              = var.subnet_id
   vpc_security_group_ids = var.security_group_ids
   iam_instance_profile   = var.instance_profile
-  user_data              = <<EOF
-#cloud-boothook
-#!/bin/bash
-sudo systemctl start nginx
-EOF
   tags = {
     Name        = var.instance_name
     Environment = "dev"
