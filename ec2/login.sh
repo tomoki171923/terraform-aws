9ab2dda03c1ad307efa80a7c4784013e6988b9ea
tomoki <tomoki171923@gmail.com>
2022-02-11 22:57:44 +0900

    delete ec2 workspace

--- ec2/login.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-BASE_PATH=`dirname $0`
-PUBLIC_IP=`terraform output -json ec2 | jq '."sample-single"[0].public_ip' | sed 's/"//g'`
-KEY_NAME=`terraform output -json ec2 | jq '."sample-single"[0].key_name' | sed 's/"//g'`
-ssh -i "~/.ssh/${KEY_NAME}.pem" ubuntu@$PUBLIC_IP
--- ec2/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-aws_region  = "ap-northeast-1"
-aws_profile = "private"
